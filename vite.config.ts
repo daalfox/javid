3cfc78696706b79318927eee9d94734f22f9aa8f
lemo10 <alireza.momenei1382@gmail.com>
2023-03-27 06:26:25 +0330

    chore: pre-config for build/publish lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +10,24 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: true,
+    port: 8080
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/entry.ts'),
+      name: 'Javid',
+      fileName: 'javid'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
   }
-})
+});
